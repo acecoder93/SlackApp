02d006894454ba3a7c075d203db907c3b106e2df
Anuj <anujsaheba@gmail.com>
2019-03-18 12:17:35 -0500

    updated chatkit instance

--- src/components/ChatScreen.js
+++ src/components/ChatScreen.js
@@ -1,12 +1,43 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Chatkit from '@pusher/chatkit-client';
 
 export default class ChatScreen extends Component {
-  render() {
-    return (
-      <div>
-          <h1>Chat</h1>
-        
-      </div>
-    )
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentUser: {}
+        }
+    }
+
+    // Right after page renders
+    componentDidMount() {
+        const chatManager = new Chatkit.ChatManager({
+            instanceLocator: '',
+            userId: this.props.currentUsername,
+            tokenProvider: new Chatkit.TokenProvider({
+                url: 'http://localhost:3001/authenticate',
+            })
+        }) // end of chatManager
+
+        chatManager
+            .connect()
+            .then(currentUser => {
+                this.setState({ currentUser })
+            })
+            .catch(error => console.log('error', error))
+    }
+
+
+
+
+
+    render() {
+        return (
+            <div>
+                <h1>Chat</h1>
+
+            </div>
+        )
+    }
 }
